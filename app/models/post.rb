ee441ae5e149734e85cb3e80464525464ad8b34a
Nicholas <nschwenne@gmail.com>
2021-10-04 10:03:15 -1000

    commit tree dc974a77c2a4bd05579798a841de99688e1253a0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction ) }
+    
+    validate :title_needs_key_words
+
+    def title_needs_key_words
+        unless title.includes?("Wont Belive", "Secret", `Top [number]`, "Guess")
+        errors.add(:title, "Needs more clickbait")    
+        end
+    end 
 end
